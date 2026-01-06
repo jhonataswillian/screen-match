21fc2b28e00817ebc4eeb97ec60c7be6861926b2
Jhonatas Willian <jhonatas.willian.dev@gmail.com>
2026-01-06 18:34:51 -0300

    Feat: add search by filter

--- src/main/java/br/com/jhonatas/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/jhonatas/screenmatch/ScreenmatchApplication.java
@@ -1,16 +1,9 @@
 package br.com.jhonatas.screenmatch;
 
-import br.com.jhonatas.screenmatch.model.DadoEpisodio;
-import br.com.jhonatas.screenmatch.model.DadoSerie;
-import br.com.jhonatas.screenmatch.model.DadoTemporada;
-import br.com.jhonatas.screenmatch.service.ConsumoApi;
-import br.com.jhonatas.screenmatch.service.ConverteDado;
+import br.com.jhonatas.screenmatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -21,31 +14,7 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados(
-				"https://www.omdbapi.com/?t=gilmore+girls&apikey=4f12b0bc"
-		);
-		System.out.println(json);
-
-		ConverteDado conversor = new ConverteDado();
-
-		DadoSerie dadosSerie = conversor.obterDado(json, DadoSerie.class);
-		System.out.println(dadosSerie);
-
-		json = consumoApi.obterDados(
-				"https://www.omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=4f12b0bc"
-		);
-		DadoEpisodio dadosEpisodio = conversor.obterDado(json, DadoEpisodio.class);
-		System.out.println(dadosEpisodio);
-
-		List<DadoTemporada> temporadas = new ArrayList<>();
-		for (int i = 1; i <= dadosSerie.totalTemporadas(); i++) {
-			json = consumoApi.obterDados(
-					"https://www.omdbapi.com/?t=gilmore+girls&season=" + i + "&apikey=4f12b0bc"
-			);
-			DadoTemporada dadoTemporada = conversor.obterDado(json, DadoTemporada.class);
-			temporadas.add(dadoTemporada);
-		}
-		temporadas.forEach(System.out::println);
+		Principal principal = new Principal();
+		principal.exibeMenu();
 	}
 }
--- src/main/java/br/com/jhonatas/screenmatch/principal/Principal.java
+++ src/main/java/br/com/jhonatas/screenmatch/principal/Principal.java
@@ -1,15 +1,27 @@
 package br.com.jhonatas.screenmatch.principal;
 
+import br.com.jhonatas.screenmatch.model.DadoEpisodio;
+import br.com.jhonatas.screenmatch.model.DadoSerie;
+import br.com.jhonatas.screenmatch.model.DadoTemporada;
+import br.com.jhonatas.screenmatch.model.Episodio;
 import br.com.jhonatas.screenmatch.service.ConsumoApi;
+import br.com.jhonatas.screenmatch.service.ConverteDado;
 
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Principal {
 
     private Scanner leitura = new Scanner(System.in);
     private ConsumoApi consumo = new ConsumoApi();
+    private ConverteDado conversor = new ConverteDado();
 
     private final String ENDERECO = "https://www.omdbapi.com/?t=";
     private final String API_KEY = "&apikey=4f12b0bc";
@@ -20,5 +32,67 @@ public class Principal {
         var nomeSerieEncoder = URLEncoder.encode(nomeSerie, StandardCharsets.UTF_8);
 
         var json = consumo.obterDados(ENDERECO + nomeSerieEncoder + API_KEY);
+
+        DadoSerie dadoSerie = conversor.obterDado(json, DadoSerie.class);
+        System.out.println(dadoSerie);
+
+        List<DadoTemporada> temporadas = new ArrayList<>();
+        for (int i = 1; i <= dadoSerie.totalTemporadas(); i++) {
+            json = consumo.obterDados(
+                    ENDERECO + nomeSerieEncoder + "&Season=" + i + API_KEY
+            );
+            DadoTemporada dadoTemporada = conversor.obterDado(json, DadoTemporada.class);
+            temporadas.add(dadoTemporada);
+        }
+
+        for (int i = 0; i < dadoSerie.totalTemporadas(); i++) {
+            List<DadoEpisodio> episodioTemporada = temporadas.get(i).episodios();
+            for (DadoEpisodio dadoEpisodio : episodioTemporada) {
+                System.out.println(dadoEpisodio.titulo());
+            }
+        }
+
+        temporadas.forEach(
+                t -> t.episodios().forEach(
+                        e -> System.out.println(e.titulo())));
+
+        List<DadoEpisodio> dadosEpisodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream())
+                .collect(Collectors.toList());
+
+        dadosEpisodios.add(new DadoEpisodio("teste", 3, "8", "2020-01-01"));
+
+        System.out.println("\nTop 5 episódios");
+        dadosEpisodios.stream()
+                .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+                .sorted(Comparator.comparing(DadoEpisodio::avaliacao).reversed())
+                .limit(5)
+                .forEach(System.out::println);
+
+        List<Episodio> episodios =  temporadas.stream()
+                .flatMap(t -> t.episodios().stream()
+                        .map(d -> new Episodio(t.numeroTemporada(), d))
+                ).collect(Collectors.toList());
+
+        //episodios.forEach(System.out::println);
+
+        System.out.println("A partir de que ano você deseja ver os episódios?");
+        var ano = leitura.nextInt();
+        leitura.nextLine();
+
+        LocalDate dataBusca = LocalDate.of(ano, 1, 1);
+
+        DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        episodios.stream()
+                .filter(e -> e.getDataLancamento() != null &&  e.getDataLancamento().isAfter(dataBusca))
+                .forEach(e -> System.out.printf("""
+                        Temporada: %d
+                        Episódio: %s
+                        Data de Lançamento: %s
+                        """,
+                        e.getTemporada(),
+                        e.getTitulo(),
+                        e.getDataLancamento().format(formatador))
+                );
     }
 }
