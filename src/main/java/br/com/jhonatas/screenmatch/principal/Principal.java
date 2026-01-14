043ea914e2b3d596aed56ff6e356b2a2c5b1f8ec
Jhonatas Willian <jhonatas.willian.dev@gmail.com>
2026-01-13 23:12:38 -0300

    Estatísticas, mapa e dados por temporada

--- src/main/java/br/com/jhonatas/screenmatch/principal/Principal.java
+++ src/main/java/br/com/jhonatas/screenmatch/principal/Principal.java
@@ -78,19 +78,19 @@ public class Principal {
 
         episodios.forEach(System.out::println);
 
-        System.out.println("Digite um trechodo titulo do episódio");
-        var trechoTitulo = leitura.nextLine();
-        Optional< Episodio> episodioBuscado = episodios.stream()
-                .filter(e -> e.getTitulo().toLowerCase().contains(trechoTitulo.toLowerCase()))
-                .findFirst();
-
-        if (episodioBuscado.isPresent()) {
-            System.out.println("Episódio encontrado!");
-            System.out.println("Temporada: " + episodioBuscado.get().getTemporada());
-            System.out.println("Episódio: " + episodioBuscado.get().getTitulo());
-        } else {
-            System.out.println("Episódio não encontrado!");
-        }
+//        System.out.println("Digite um trechodo titulo do episódio");
+//        var trechoTitulo = leitura.nextLine();
+//        Optional< Episodio> episodioBuscado = episodios.stream()
+//                .filter(e -> e.getTitulo().toLowerCase().contains(trechoTitulo.toLowerCase()))
+//                .findFirst();
+//
+//        if (episodioBuscado.isPresent()) {
+//            System.out.println("Episódio encontrado!");
+//            System.out.println("Temporada: " + episodioBuscado.get().getTemporada());
+//            System.out.println("Episódio: " + episodioBuscado.get().getTitulo());
+//        } else {
+//            System.out.println("Episódio não encontrado!");
+//        }
 //
 //
 //        System.out.println("A partir de que ano você deseja ver os episódios?");
@@ -111,5 +111,21 @@ public class Principal {
 //                        e.getTitulo(),
 //                        e.getDataLancamento().format(formatador))
 //                );
+
+        Map<Integer, Double> avaliacoesPorTemporada = episodios.stream()
+                .filter(e -> e.getAvaliacao() != null && e.getAvaliacao() > 0.0)
+                .collect(Collectors.groupingBy(Episodio::getTemporada,
+                        Collectors.averagingDouble(Episodio::getAvaliacao)));
+
+        System.out.println(avaliacoesPorTemporada);
+
+        DoubleSummaryStatistics est = episodios.stream()
+                .filter(e -> e.getAvaliacao() != null && e.getAvaliacao() > 0.0)
+                .collect(Collectors.summarizingDouble(Episodio::getAvaliacao));
+
+        System.out.println("Média: " + est.getAverage() +
+                "\nMelhor episódio: " + est.getMax() +
+                "\nPior episódio: " + est.getMin() +
+                "\nQuantidade: " + est.getCount());
     }
 }
